69df7e8985b0d21282400135a168d5e505a464e0
Paula Augustin <samuel.robert.augustin@gmail.com>
2020-11-02 19:57:57 +0200

    good

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('products\index');
+    return view('products');
 });
 
 use App\Http\Controllers\ProductController;
